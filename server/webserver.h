63ccd5de1750c683dbb71390f1a67892b563681c
wrdoct <llw18729357726@163.com>
2022-04-19 21:45:55 +0800

    server

---
+++ server/webserver.h
@@ -0,0 +1,20 @@
+/*
+ * @Author       : llw
+ * @Date         : 2022-04-20
+ */ 
+#ifndef WEBSERVER_H
+#define WEBSERVER_H
+
+#include <unordered_map>
+#include <fcntl.h>       // fcntl()
+#include <unistd.h>      // close()
+#include <assert.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+
+
+#endif //WEBSERVER_H
