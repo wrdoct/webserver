78be3e7379ab342bb4f6908f2642678dab73395b
wrdoct <llw18729357726@163.com>
2022-04-20 20:27:07 +0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
  * @Author       : llw
  * @Date         : 2022-04-20
  */ 
+
 #include <unistd.h>
 #include "server/webserver.h"
 
@@ -10,9 +11,9 @@ int main(){
     //daemon(1, 0); 
 
     WebServer server(
-        1316, 3, false,         /* 端口 ET模式 优雅退出  */
-        12, 6,                  /* 连接池数量 线程池数量 */
-        true, 1, 1024           /* 日志开关 日志等级 日志异步队列容量 */
+        1316, 3,            /* 端口 ET模式 */
+        12, 6,              /* 连接池数量 线程池数量 */
+        true, 1, 1024       /* 日志开关 日志等级 日志异步队列容量 */
         );
     server.Start();
 
