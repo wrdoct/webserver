fbfb998e99544b3e910ee608b4833d2976da6413
wrdoct <llw18729357726@163.com>
2022-04-20 20:31:41 +0800

    webserver.h

--- server/webserver.h
+++ server/webserver.h
@@ -1,7 +1,3 @@
-/*
- * @Author       : llw
- * @Date         : 2022-04-20
- */ 
 #ifndef WEBSERVER_H
 #define WEBSERVER_H
 
@@ -15,13 +11,15 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include "../log/log.h"
 #include "epoller.h"
 #include "threadpool.h"
+#include "../http/httpconn.h"
 
 class WebServer {
 public:
     WebServer(
-        int port, int trigMode, bool OptLinger, 
+        int port, int trigMode,
         int connPoolNum, int threadNum,
         bool openLog, int logLevel, int logQueSize);
 
@@ -34,32 +32,31 @@ private:
     void AddClient_(int fd, sockaddr_in addr);
   
     void DealListen_();
-    // void DealWrite_(HttpConn* client);
-    // void DealRead_(HttpConn* client);
+    void DealWrite_(HttpConn* client);
+    void DealRead_(HttpConn* client);
 
-    // void SendError_(int fd, const char*info);
-    // void CloseConn_(HttpConn* client);
+    void SendError_(int fd, const char*info);
+    void CloseConn_(HttpConn* client);
 
-    // void OnRead_(HttpConn* client);
-    // void OnWrite_(HttpConn* client);
-    // void OnProcess(HttpConn* client);
+    void OnRead_(HttpConn* client);
+    void OnWrite_(HttpConn* client);
+    void OnProcess(HttpConn* client);
 
-    static const int MAX_FD = 65536; //最大的文件描述符的个数
+    static const int MAX_FD = 65536; 
 
-    static int SetFdNonblock(int fd); //设置文件描述符非阻塞
+    static int SetFdNonblock(int fd); 
 
-    int port_; //端口
-    bool openLinger_; //是否打开优雅关闭
+    int port_; 
     bool isClose_; //是否关闭
     int listenFd_; //监听的文件描述符
     char* srcDir_; //资源的目录
     
-    uint32_t listenEvent_; //监听的文件描述符的事件
-    uint32_t connEvent_; //连接的文件描述符的事件
+    uint32_t listenEvent_; 
+    uint32_t connEvent_; 
    
     std::unique_ptr<ThreadPool> threadpool_; //线程池
     std::unique_ptr<Epoller> epoller_; //epoll对象
-    //std::unordered_map<int, HttpConn> users_; //保存的是客户端连接的信息（哈希表：文件描述符-http连接）
+    std::unordered_map<int, HttpConn> users_; //保存的是客户端连接的信息（哈希表：文件描述符-http连接）
 };
 
 
