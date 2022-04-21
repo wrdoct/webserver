a8e667fea7c4b529e00e943123b788b0c9a0c9b9
wrdoct <llw18729357726@163.com>
2022-04-21 20:48:42 +0800

    buffer.cpp

--- buffer/buffer.cpp
+++ buffer/buffer.cpp
@@ -19,22 +19,28 @@ const char* Buffer::Peek() const{
 }
 
 void Buffer::EnsureWriteable(size_t len){
-
+    if(WritableBytes() < len) {
+        MakeSpace_(len); //对容器的操作（扩容 或者 拷贝移动数据）
+    }
+    assert(WritableBytes() >= len);
 }
 
 void Buffer::HasWritten(size_t len){
-
+    writePos_ += len;
 }
 
 void Buffer::Retrieve(size_t len){
-
+    assert(len <= ReadableBytes());
+    readPos_ += len;
 }
 void Buffer::RetrieveUntil(const char* end){
 
 }
 
 void Buffer::RetrieveAll(){
-
+    bzero(&buffer_[0], buffer_.size()); 
+    readPos_ = 0;
+    writePos_ = 0;
 } 
 std::string Buffer::RetrieveAllToStr(){
 
@@ -51,7 +57,10 @@ void Buffer::Append(const std::string& str){
 
 }
 void Buffer::Append(const char* str, size_t len){
-
+    assert(str);
+    EnsureWriteable(len); //这里的len是临时数组中的数据个数
+    std::copy(str, str + len, BeginWrite());
+    HasWritten(len);
 }
 void Buffer::Append(const void* data, size_t len){
 
@@ -60,10 +69,31 @@ void Buffer::Append(const Buffer& buff){
 
 }
 
-ssize_t Buffer::ReadFd(int fd, int* Errno){
+ssize_t Buffer::ReadFd(int fd, int* saveErrno){
+    char buff[65535]; //临时数组
+    struct iovec iov[2]; //定义了一个向量元素  分散的内存
+    const size_t writable = WritableBytes();
+    /* 分散读， 保证数据全部读完 */ 
+    iov[0].iov_base = BeginPtr_() + writePos_;
+    iov[0].iov_len = writable; 
+    iov[1].iov_base = buff;
+    iov[1].iov_len = sizeof(buff);
 
+    const ssize_t len = readv(fd, iov, 2); 
+    if(len < 0) {
+        *saveErrno = errno;
+    }
+    else if(static_cast<size_t>(len) <= writable) {
+        writePos_ += len;
+    }
+    else {
+        writePos_ = buffer_.size();
+        Append(buff, len - writable);//把剩下的数据做处理（继续放在当前容器里或者是扩容）
+    }
+
+    return len;
 }
-ssize_t Buffer::WriteFd(int fd, int* Errno){
+ssize_t Buffer::WriteFd(int fd, int* saveErrno){
 
 }
 
@@ -77,13 +107,13 @@ const char* Buffer::BeginPtr_() const {
 
 void Buffer::MakeSpace_(size_t len) {
     if(WritableBytes() + PrependableBytes() < len) { //剩余可写的大小 加 前面可用的空间 小于 临时数组中的长度
-        buffer_.resize(writePos_ + len + 1); //调整容器大小
+        buffer_.resize(writePos_ + len + 1);
     } 
     else { //可以装len长度的数据 就直接将后面的数据拷贝到前面
-        size_t readable = ReadableBytes(); //可以读的数据的大小
-        std::copy(BeginPtr_() + readPos_, BeginPtr_() + writePos_, BeginPtr_());//把后面的未读数据拷贝到前面 //std::copy(start, end, std::back_inserter(container)); 
-        readPos_ = 0; //恢复读的初始位置为0
-        writePos_ = readPos_ + readable; //已写了的位置等于 读的初始位置0 加上 之前剩余未读（可以读）的数据
+        size_t readable = ReadableBytes(); 
+        std::copy(BeginPtr_() + readPos_, BeginPtr_() + writePos_, BeginPtr_());
+        readPos_ = 0; 
+        writePos_ = readPos_ + readable; 
         assert(readable == ReadableBytes());
     }
 }
