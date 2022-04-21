f88887373e0723fc9055d362603bca7e71b823f1
wrdoct <llw18729357726@163.com>
2022-04-21 20:46:49 +0800

    blockqueue.h

---
+++ log/blockqueue.h
@@ -0,0 +1,18 @@
+#ifndef BLOCKQUEUE_H
+#define BLOCKQUEUE_H
+
+#include <mutex>
+#include <deque>
+#include <condition_variable>
+#include <sys/time.h>
+
+template<class T>
+class BlockDeque {
+public:
+    explicit BlockDeque(size_t MaxCapacity = 1000);
+
+    ~BlockDeque();
+
+};
+
+#endif // BLOCKQUEUE_H
