96585a83c0d4045e90859aa9112f2ccf449de27f
llw <llw18729357726@163.com>
2022-04-24 14:37:35 +0800

    httpconn.h

--- http/httpconn.h
+++ http/httpconn.h
@@ -4,7 +4,7 @@
 #include <sys/types.h>
 #include <sys/uio.h>     // readv/writev
 #include <arpa/inet.h>   // sockaddr_in
-#include <stdlib.h>      // atoi()
+#include <stdlib.h>      
 #include <errno.h>  
 
 #include "../log/log.h"
