68ad8d786ca8a1379f857ab9eede13c037b3f405
llw <llw18729357726@163.com>
2022-04-24 14:40:24 +0800

    buffer.h

--- buffer/buffer.h
+++ buffer/buffer.h
@@ -26,18 +26,15 @@ public:
     void RetrieveUntil(const char* end);
 
     void RetrieveAll() ; 
-    std::string RetrieveAllToStr();
 
     const char* BeginWriteConst() const;
     char* BeginWrite();
 
     void Append(const std::string& str);
     void Append(const char* str, size_t len);
-    void Append(const void* data, size_t len);
-    void Append(const Buffer& buff);
 
     ssize_t ReadFd(int fd, int* Errno);
-    ssize_t WriteFd(int fd, int* Errno);
+    //ssize_t WriteFd(int fd, int* Errno);
 
 private:
     char* BeginPtr_(); 
